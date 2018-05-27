7baa074977da6d35ba5e3436ed2ee9e4fe449946
Emad Elsaid <blazeeboy@gmail.com>
2018-05-27 15:20:44 +0200

    add leader keys for the notification open function

--- keybindings.el
+++ keybindings.el
@@ -1,3 +1,6 @@
 (when (fboundp 'evil-define-minor-mode-key)
   (evil-define-minor-mode-key 'normal 'notifications-mode (kbd "q") 'notifications-close)
   (evil-define-minor-mode-key 'normal 'notifications-mode (kbd "r") 'notification-mark-read))
+
+(spacemacs/declare-prefix "an" "Notifications")
+(spacemacs/set-leader-keys "ann" 'notifications-open)
